babd65eb881448c4bdb6efce85a865442b990b24
Sofia-Palacios-Orrego <133055759+Sofia-Palacios-Orrego@users.noreply.github.com>
2023-05-25 11:15:48 -0400

    Add files via upload

---
+++ Palacios_Sofia_Me Gusta/script.js
@@ -0,0 +1,29 @@
+
+function addLike(){
+
+    let spanNumLikes = document.querySelector (".numLikes")
+    numLikes = Number(spanNumLikes.textContent);
+    numLikes ++;
+    spanNumLikes.innerText = numLikes;
+
+}
+
+
+function addLikeSec(){
+
+    let spanNumLikesS = document.querySelector (".numLikesS")
+    numLikesS= Number(spanNumLikesS.textContent);
+    numLikesS ++;
+    spanNumLikesS.innerText = numLikesS;
+
+}
+
+
+function addLikeThi(){
+
+    let spanNumLikesT = document.querySelector (".numLikesT")
+    numLikesT= Number(spanNumLikesT.textContent);
+    numLikesT ++;
+    spanNumLikesT.innerText = numLikesT;
+
+}
